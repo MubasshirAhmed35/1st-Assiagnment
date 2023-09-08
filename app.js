47c7b78287a5014bcf16598814d572f0871abf18
Mubasshir Ahmed <mubashirsarkar35@gmail.com>
2023-09-08 15:19:42 +0500

    app.js

---
+++ app.js
@@ -0,0 +1,90 @@
+// chapter 1
+
+// Q 2
+alert("Error!Please enter a valid password")
+
+// Q 3
+alert("Welcom to JS Land... \n Happy Coding!")
+
+// Q 4
+alert("Welcom to JS Land...")
+alert("Happy Coding!")
+
+// Q 5
+alert("Hello...I can run my JS through my web browser's console")
+
+
+
+
+// chapter 2
+
+// Q 1
+var userName = "mubasshir";
+
+// Q 2
+var myName = "Mubasshir Ahmed";
+
+// Q 3
+var message;
+message = "Hello World";
+
+// Q 4
+var name = "Mubasshir Ahmed";
+
+var age = "19";
+
+alert("Mubasshir Ahmed")
+
+alert("age 19")
+
+// Q 5
+alert(" PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+// Q 6
+var email = "mubashirsarkar35@gmail.com";
+
+alert("My email address is mubashirsarkar35@gmail.com")
+
+// Q 7
+var book = "A smarter way to learn JavaScript";
+
+alert("I am trying to learn from the Book A smarter way to learn JavaScript")
+
+// Q 8
+document.write("Yah! I can write HTML content through JavaScript")
+
+// Q 9
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+
+// chapter 3
+
+// Q 1
+var age = 19;
+
+alert("I am 19 years old")
+
+// Q 2
+alert("You have visited this site 14 times")
+
+// Q 3
+var birthYear = 2004;
+
+document.write("My birth year is 2004 <br> Data type of my declared variable is number")
+
+// Q 4
+var name = "Mubasshir"
+
+var title = "T-shirts"
+
+var quantity = 5
+
+document.write("Mubasshir ordered 5 T-shirt(s) on XYZ Clothing store.")
+
+
+
+
+// chapter 5
+
+// Q 1
